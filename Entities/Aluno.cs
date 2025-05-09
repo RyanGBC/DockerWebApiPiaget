1a7cbd77562c9b0a17a40a3ab6134389a8371925
Aluno <Aluno@UPGT-SPBR-PC152>
2025-05-09 10:42:25 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Aluno
+    {
+        public Guid id { get; set; }
+        public string? Nome { get; set; }
+        public string? Escola { get; set; }
+        public string? Professor { get; set; }
+    }
+
+}
